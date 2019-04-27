f10d52df15b2d40410608b1be0d5a949162cd87b
Peter N Jenkin <peternjenkin@gmail.com>
2019-04-27 13:34:16 +0100

    33 Overflow menu with radio-button style toggle to select colour for screen background

--- app/src/main/java/com/example/overflowmenu33/MainActivity.java
+++ app/src/main/java/com/example/overflowmenu33/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.overflowmenu33;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,18 +38,48 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // NB called when any overflow menu item selected
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        ConstraintLayout main_view = (ConstraintLayout) findViewById(R.id.main_view_layout);     // get reference to background (pre changing its colour)
+        // this ought to have been a RelativeLayout but I forgot and was too lazy to change this (TODO)
+
+        switch(item.getItemId())
+        {
+            case R.id.menu_red:
+//                if (item.isChecked())
+//                {
+//                    item.setChecked(false);
+//                }
+//                else
+//                {
+//                    item.setChecked(true);
+//                }
+                item.setChecked(!item.isChecked());
+                main_view.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu_green:
+                item.setChecked(!item.isChecked());
+                main_view.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.menu_blue:
+                item.setChecked(!item.isChecked());
+                main_view.setBackgroundColor(Color.BLUE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);       // from boilerplate
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
